990ca9494f89fdfad453381360e3454bf97878b6
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-09-11 15:29:07 +1000

    Added scripts

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(MazeGenerator))]
+
+public class GameController : MonoBehaviour
+{
+    private MazeGenerator generator;
+
+    void Start()
+    {
+        generator = GetComponent<MazeGenerator>();
+    }
+}
